7b650166f43c6383a5a57a34aeab173c7265fa1e
Hamzah Rushdan <hamzahrushdan79@gmail.com>
2022-12-11 19:28:05 -0500

    correct answers

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+//if distance is less than 400 display: give customers a free sample 
+//how to setup distance or entry point?
+//let distance = 400
+
+if (distance <= 400) {
+  
+ return "This one is on me!"
+  }else if (distance > 400 && distance < 2000){
+    return "That will be twenty bucks."
+  }else if (distance > 2000 && distance < 2500){
+    return "I will gladly take your thirty bucks."
+  }else {
+    return "No can do."
+  }
+  
+}
+scuberGreetingForFeet(300);
+
+
+//scuberGreetingForFeet(distance);
+function ternaryCheckCity(city){
+if (city === "NYC"){
+  return "Ok, sounds good."
+}else {
+  return "No go."
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return "Thank you so much."
+  }else if (tip === "not as generous"){
+    return "Thank you."
+  }else {
+    return "Bye."
+  }
+  
 }
